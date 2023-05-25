aa71b57464fbc9b3a75442c3a57e01ee44728fdc
rossvasquez <ross.vasquez.17@gmail.com>
2023-05-24 23:11:10 -0500

    Updates

--- src/assets/components/contact/topText.jsx
+++ src/assets/components/contact/topText.jsx
@@ -10,13 +10,13 @@ function TopText() {
             <p className='relative font-staat text-5xl md:text-6xl text-amber-300 textShadow5 tracking-wide z-10 text-center'>Contact Us</p>
             <img className='absolute opacity-10 -top-10 -right-4 sm:right-0 md:-top-24 sm:-mt-14 w-full min-w-[40rem] blur-[2px]' src={contactCover} />
         </div>
-        <div className="relative flex flex-col justify-center h-auto w-screen mx-auto px-4 overflow-hidden mt-12">
+        <div className="relative flex flex-col justify-center items-center h-auto w-screen mx-auto px-4 overflow-hidden mt-12">
             <p className='text-center text-7xl sm:text-8xl font-slab text-slate-50 font-bold 
             textShadow7 leading-tight md:leading-tight uppercase'>
                 What's On Your Mind?
             </p>
             <p className='mt-6 uppercase text-3xl md:text-4xl text-slate-800 max-w-6xl mx-auto 
-            font-staat leading-snug md:leading-snug tracking-wide md:tracking-wide'>
+            font-staat leading-snug md:leading-snug tracking-wide md:tracking-wide text-center'>
                 Questions, Comments, or Concerns - we'd love to hear from you!
             </p>
         </div>
